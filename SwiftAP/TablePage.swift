e9c3fbfe2707e539636f7087d1c45c54ecb2dd26
abcd1234davidchen <abcd1234davidchen@gmail.com>
2024-08-24 01:12:53 +0800

    Modify Table Page

--- SwiftAP/TablePage.swift
+++ SwiftAP/TablePage.swift
@@ -11,11 +11,22 @@ struct TablePage: View {
     var body: some View {
         NavigationStack(){
             HStack{
-                DayInWeek
-                DayInWeek
+                DayView(day: "Mon")
+                DayView(day: "Tue")
             }
             
             .navigationTitle("Table")
+            
+            .toolbar{
+                ToolbarItem(){
+                    Button(action: {}, label: {
+                        Label(
+                            title: { Text("Label") },
+                            icon: { Image(systemName: "rectangle.portrait.and.arrow.forward") }
+                        )
+                    })
+                }
+            }
         }
     }
 }
@@ -24,11 +35,35 @@ struct TablePage: View {
     TablePage()
 }
 
-
-let DayInWeek: some View = VStack{
-    Text("Mon").font(.title.bold()).padding([.all],5).minimumScaleFactor(0.5).lineLimit(1)
-    VStack {
-        Text("Test").font(.title).padding([.all],5).lineLimit(1)
-        Text("Score").font(.subheadline).padding([.all],5).lineLimit(1)
+struct CourseView: View {
+    var name: String
+    var additonal: String
+    
+    var body: some View{
+        VStack {
+            Text(name)
+                .font(.title)
+                .padding(5)
+                .lineLimit(1)
+            Text(additonal)
+                .font(.subheadline)
+                .padding(5)
+                .lineLimit(1)
+        }
+        .frame(width: 100, height: 100)
+        .background(Color.gray)
+        .clipShape(RoundedRectangle(cornerRadius: 20.0))
+        
     }
-}.minimumScaleFactor(0.5)
+}
+
+struct DayView: View{
+    var day: String
+    
+    var body: some View{
+        VStack{
+            Text(day).font(.title.bold()).padding([.all],5).minimumScaleFactor(0.5).lineLimit(1)
+            CourseView(name: "Test",additonal: "What")
+        }
+    }
+}
